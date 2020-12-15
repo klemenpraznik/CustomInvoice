4d893307d882d81fb54aa36e6b7a9f8691594fb7
Klemen Praznik <klemen.prazo@gmail.com>
2020-12-15 15:28:31 +0100

    production

--- CustomInvoice.WebApp/Startup.cs
+++ CustomInvoice.WebApp/Startup.cs
@@ -32,7 +32,7 @@ namespace CustomInvoice.WebApp
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("AzureConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
